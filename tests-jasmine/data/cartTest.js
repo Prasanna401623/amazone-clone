8096caff212b39923c9365e030bb2e60f2933b93
Prasanna Jha <prasannajha401@gmail.com>
2024-07-25 01:01:07 -0400

    Update atuomated tests for addToCart to make it workable

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -4,18 +4,71 @@
 // This is the code I could write and work and I also used chatgpt to clear some things. For this test, I will have to come back again. 
 
 
-import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
+import { addToCart, loadFromStorage, cart } from "../../data/cart.js";
 
+
+// describe function is provided by jasmine. It used to test suite i.e, group of tests. It takes two parameters. The first parameter is the name of test suite (string), and second parameter is the function (the test you want to do).
 describe('test suite: addToCart', () => {
-  beforeEach(() => {
-    // Clear the cart before each test
-    cart.length = 0;
 
-    // Mocking localStorage methods
+  
+  let originalLocalStorage;
+
+
+  // beforeEach function is used to create codes that is applied before each test. This is done so that there is no leakage of test code that could affect main project. 
+  beforeEach(() => {
+
+    // Backup the original localStorage. This code creates a backup of the current state of localStorage. It copies all of 'localStorage' properties into new object called originalLocalStorage. This is done so we can restore the original state later.
+    originalLocalStorage = { ...localStorage };
+
+    // mockLocalStorage is assigned to a function.
+    const mockLocalStorage = (function() {
+
+      // store is assigned to empty object. It is used to simulate the 'localStorage' interface.
+      let store = {};
+
+      // the function returns the object that mimic the functionality of 'localStorage'. The object has getItem and setItem. Key is used to save and retrive the data. 
+      return {
+        getItem: function(key) {
+          return store[key] || null;
+        },
+        setItem: function(key, value) {
+          store[key] = value.toString();
+        },
+        clear: function() {
+          store = {};
+        }
+      };
+    })();
+
+    // Replace the global localStorage with the mock
+    Object.defineProperty(window, 'localStorage', {
+      value: mockLocalStorage,
+      writable: true
+    });
+
+
+  });
+
+  // afterEach function is used to write to code that can be applied after each test. In this case, after each test the original localStorage is restored. 
+  afterEach(() => {
+    
+    // Restore the original localStorage
+    Object.defineProperty(window, 'localStorage', {
+      value: originalLocalStorage,
+      writable: true
+    });
+  });
+
+  // it function is used to create test single section. It takes two parameters. The first parameter is the name of test you want to do (string) and the second parameter is function that contains the test code you want to use. 
+  it('adds a new product in the cart', () => {
+
+    // spyOn is used to create a fake version of localStorage.setItem so the testing code doesn't interfere with main program. 
+    spyOn(localStorage, 'setItem');
+
+    // spyOn helps us to create a  fake version of localStorage.getItem. And that fake version returns an empty.
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([]);
-    });
-    spyOn(localStorage, 'setItem');
+    })
 
     // Mocking document.querySelector to return a fake element with the desired value
     spyOn(document, 'querySelector').and.callFake((selector) => {
@@ -27,14 +80,61 @@ describe('test suite: addToCart', () => {
 
     // Load the cart from storage (should be empty because of the mock)
     loadFromStorage();
-  });
 
-  it('adds an existing product to the cart', () => {
     // Call the function to add the product to the cart
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
 
     // Check if the cart contains the product
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+
+    // Check of the length of cart i.e, the amount of product in the cart equals 1.
+    expect(cart.length).toEqual(1);
+
+    // Check if the localStorage.setItem has been called once. This expectation can only be used when spyOn is used. 
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+
+    // check if the first product's quantity equals 1.
+    expect(cart[0].quantity).toEqual(1);
+  });
+
+  it('adds an existing product in the cart', () => {
+
+    // spyOn is used to create a fake version of localStorage.setItem so the testing code doesn't interfere with main program. 
+    spyOn(localStorage, 'setItem');
+
+    // spyOn helps us to create a  fake version of localStorage.getItem. And that fake version returns an empty 
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId:'1'
+      }]);
+    });
     
+    // Mocking document.querySelector to return a fake element with the desired value
+    spyOn(document, 'querySelector').and.callFake((selector) => {
+      if (selector === '.js-quantity-selector-e43638ce-6aa0-4b85-b27f-e1d07eb678c6') {
+        return { value: '1' };
+      }
+      return null;
+    });
+
+    // Load the cart from storage (should be empty because of the mock)
+    loadFromStorage();
+
+    // Call the function to add the product to the cart
+    addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+
+    // Check if the cart contains the product
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+
+    // Check of the length of cart i.e, the amount of product in the cart equals 1.
+    expect(cart.length).toEqual(1);
+
+    // Check if the localStorage.setItem has been called once. This expectation can only be used when spyOn is used. 
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+
+    // check if the first product's quantity equals 2.
+    expect(cart[0].quantity).toEqual(2);
   });
 });
