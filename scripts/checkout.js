9f829facc4b7396851fdbc70be48a6445ab003b0
Prasanna Jha <prasannajha401@gmail.com>
2024-08-05 07:00:53 -0400

    Convert call back to Promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,10 +4,23 @@ import { loadProducts } from '../data/products.js';
 // import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
+// Promise is a class. When we creat promise,we need to give it a function. It runs the inner function immediately. That means loadProducts runs immediately. resolve is similar to done(). Once the resolve is used, the next step will run. To run the next step, we will use then(). then() takes a function as a parameter. 
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+  
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+})
+
+/*
 // loadProducts has been imported and given an function as parameter. The arrow function has two more functions. What this does is, it runs loadProducts() first and within the loadProducs there is function that has been used as parameter. The arrow function will be used in place of that function. 
 loadProducts(() => {
-// calling renderOrderSummary()
-renderOrderSummary();
+  // calling renderOrderSummary()
+  renderOrderSummary();
 
-renderPaymentSummary();
+  renderPaymentSummary();
 })
+*/
